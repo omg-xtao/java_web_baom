aa27917be61b8a9ffd8a670f4077e8a0be338075
xtaodada <xtao@xtaolink.cn>
2023-12-21 14:59:29 +0800

    feat: listener

---
+++ src/main/java/com/listener/CountListener.java
@@ -0,0 +1,28 @@
+package com.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * @author xtaod
+ */
+public class CountListener implements HttpSessionListener {
+    @Override
+    public void sessionCreated(javax.servlet.http.HttpSessionEvent se) {
+        ServletContext context = se.getSession().getServletContext();
+        if (context.getAttribute("count") == null) {
+            Integer count = 1;
+            context.setAttribute("count", count);
+        } else {
+            Integer count = (Integer) context.getAttribute("count");
+            context.setAttribute("count", count + 1);
+        }
+    }
+
+    @Override
+    public void sessionDestroyed(javax.servlet.http.HttpSessionEvent se) {
+        ServletContext context = se.getSession().getServletContext();
+        Integer count = (Integer) context.getAttribute("count");
+        context.setAttribute("count", count - 1);
+    }
+}
--- src/main/webapp/admin/state.jsp
+++ src/main/webapp/admin/state.jsp
@@ -5,12 +5,16 @@
   Time: 14:11
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
-
+<%
+    ServletContext context= session.getServletContext();
+    Integer count = (Integer) context.getAttribute("count");
+%>
+<p>当前在线人数共 <%=count %> 人</p>
 </body>
 </html>
